1fb732f970ca9703bebe2f612583fb01a37050a6
任阳 <9520501+ren-yang001@user.noreply.gitee.com>
2023-09-05 17:12:00 +0800

    安装ElementPlus和Tailwind库

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-32 h-32 bg-blue-600">
+    <h1 class="">This is an about page</h1>
+  </div>
+</template>
+
+<style>
+div {
+  border-top: 1px solid theme('colors.gray.200');
+}
+</style>
